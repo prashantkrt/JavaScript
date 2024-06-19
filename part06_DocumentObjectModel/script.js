9dd051aa4b8d465b0ecbebfafa8c084667dbdaa6
prashanttiwary10@gmail.com <prashanttiwary10@gmail.com>
2024-06-19 21:43:45 +0530

    DOM

---
+++ part06_DocumentObjectModel/script.js
@@ -0,0 +1,11 @@
+alert("Hello");
+console.log(window)
+window.console.log("hello world")
+console.dir(window.document.body.childNodes[0]);
+
+document.body.style.background="yellow";
+
+window.document.body.childNodes[1].innerText ="my text"
+
+let heading = document.getElementById("header1");
+console.dir(heading)
