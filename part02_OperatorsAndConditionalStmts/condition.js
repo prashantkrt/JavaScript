cd8cc8e9dcb2254e4faa3910f6b47277597a2f9e
prashanttiwary10@gmail.com <prashanttiwary10@gmail.com>
2024-06-19 13:21:08 +0530

    conditions and operations

---
+++ part02_OperatorsAndConditionalStmts/condition.js
@@ -0,0 +1,18 @@
+let age = 25;
+if (age > 18) {
+  console.log("Can vote");
+} else console.log("not eligible to vote");
+
+let time = 20;
+
+if (time < 10) {
+  greeting = "Good morning";
+} else if (time < 20) {
+  greeting = "Good day";
+} else {
+  greeting = "Good evening";
+}
+
+
+let ans = age>18 ? "can vote" : "cannot vote";
+console.log(ans);
---
+++ part02_OperatorsAndConditionalStmts/operations.js
@@ -0,0 +1,26 @@
+let a =10;
+let b= 20;
+
+console.log("a=",a, "& b=",b);
+console.log("a-b",a-b);
+console.log("a+b",a+b);
+console.log("a*b",a*b);
+console.log("a/b",a/b);
+console.log("a%b",a%b);
+console.log("a ^ b",a**b);
+console.log(a++);
+console.log(b--);
+a+=4;
+console.log(a)
+
+console.log(a==b);
+console.log(a!=b);
+
+let x = 10; 
+let y = "10";
+console.log(x==y);//true
+console.log(x===y);//also checks typeOf
+
+let u = 10;
+let v = "10";
+console.log(u!==v);
