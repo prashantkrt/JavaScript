8968bcd59e0733e6b66808b033af989faa639d66
prashanttiwary10@gmail.com <prashanttiwary10@gmail.com>
2024-06-23 15:47:53 +0530

    fetch api

--- part013_FetchAPI/app.js
+++ part013_FetchAPI/app.js
@@ -1,7 +1,29 @@
 const url = "https://cat-fact.herokuapp.com/facts";
+const factPara = document.querySelector("#fact");
+const factButton = document.querySelector("#getFact");
 
 const getFacts = async () => {
   console.log("getting data ....");
-  let promise = await fetch(url);
-  console.log(promise);
+  let response = await fetch(url); //gives promise
+  console.log(response); //JSON Format
+  console.log(response.status);
+  let data = await response.json(); // returns the promise
+  console.log(data);
+  console.log(data[0]);
+  console.log(data[0].text);
+  factPara.innerText = data[0].text;
 };
+
+factButton.addEventListener("click", getFacts);
+
+
+//promise chaining 
+function getTheFacts() {
+  fetch(url)
+    .then((response) => {
+      return response;
+    })
+    .then((data) => {
+      console.log(data);
+    });
+}
